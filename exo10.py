09a1b5894ea861030df0296ff83b060111af4168
laynaria <pro.laynaria@outlook.fr>
2025-12-15 15:21:47 +0100

    finished exo10

---
+++ exo10.py
@@ -0,0 +1,12 @@
+# À partir des variables suivantes, et sans les modifier, formater la chaîne suivante pour obtenir le résultat suivant :
+# "Bonjour, je m'appelle Bobby, j'ai 0030 ans, je suis né le May 26 1992."
+# Printer la phrase
+from datetime import date 
+prenom = "bobby"
+age = 30
+date_naissance = date(1992, 5, 26)
+
+phrase = f"Bonjour, je m'appelle {prenom.capitalize()}, j'ai {age:04d} ans, je suis né le {date_naissance:%B %d %Y}."
+
+print(phrase)
+print(phrase == "Bonjour, je m'appelle Bobby, j'ai 0030 ans, je suis né le May 26 1992.")

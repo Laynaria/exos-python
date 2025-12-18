d9c09cfa4c7084ebe881883bbdf5cc86289f4ef0
laynaria <pro.laynaria@outlook.fr>
2025-12-18 16:34:15 +0100

    OOP exo5: created files, and book class done.

---
+++ oop/exo5.py/book.py
@@ -0,0 +1,43 @@
+class Book:
+    def __init__(self, title, author, publication_year):
+        self.title = title
+        self.author = author
+        self.publication_year = publication_year
+
+    # Getters / Setters
+    @property
+    def title(self) -> int:
+        return self._title
+    
+    @title.setter
+    def title(self, new_title) -> None:
+        self._title = new_title
+
+    @property
+    def author(self) -> int:
+        return self._author
+    
+    @author.setter
+    def author(self, new_author) -> None:
+        self._author = new_author
+
+    @property
+    def publication_year(self) -> int:
+        return self._publication_year
+    
+    @publication_year.setter
+    def publication_year(self, new_publication_year) -> None:
+        self._publication_year = new_publication_year
+
+
+    # Methods
+    def show_details(self):
+        print(f"Author: {self.author}")
+        print(f"Title: {self.title}")
+        print(f"Year: {self.publication_year}")
+
+    # Magic Methods
+
+    def __str__(self):
+        print(f"Book : {self.title} written by {self.author} published in {self.publication_year}")
+

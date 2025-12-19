326a59578e4bdd397c6636b170cfeb268ddb607e
laynaria <pro.laynaria@outlook.fr>
2025-12-19 10:05:04 +0100

    added getter and setter for visibility of available property

--- oop/exo5/borrowable_book.py
+++ oop/exo5/borrowable_book.py
@@ -5,6 +5,14 @@ class BorrowableBook(Book):
         super().__init__(title, author, publication_year)
         self.available = True
 
+    @property
+    def available(self) -> bool:
+        return self.__available
+
+    @available.setter
+    def available(self, new_availability):
+        self.__available = new_availability
+
     def show_details(self):
         print(f"Author: {self.author}")
         print(f"Title: {self.title}")
